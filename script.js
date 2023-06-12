31adedad2f0f87b45c43d7dc1dbfe1ebd50d0f52
Akshatadhale <117210713+Akshatadhale@users.noreply.github.com>
2023-06-12 22:56:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function convertToFahrenheit() {
+    var celsiusInput = document.getElementById('celsius-input');
+    var fahrenheitInput = document.getElementById('fahrenheit-input');
+  
+    var celsius = parseFloat(celsiusInput.value);
+    var fahrenheit = (celsius * 9/5) + 32;
+  
+    fahrenheitInput.value = fahrenheit.toFixed(2);
+  }
+  
+  function convertToCelsius() {
+    var celsiusInput = document.getElementById('celsius-input');
+    var fahrenheitInput = document.getElementById('fahrenheit-input');
+  
+    var fahrenheit = parseFloat(fahrenheitInput.value);
+    var celsius = (fahrenheit - 32) * 5/9;
+  
+    celsiusInput.value = celsius.toFixed(2);
+  }
+  
